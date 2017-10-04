ad591f635c63331d34ac2ecb1eda15f1ebc9e200
Daniel Caldera García <daniel@Daniels-MacBook-Pro.local>
2017-10-04 16:35:47 +0200

    feat: add login and userlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import * as fromRoot from '../reducers';
 import * as category from '../actions/categories';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AuthProvider } from './../providers/auth.provider';
+import { LoginPage } from './../pages/login/login';
+import { UsersListPage } from '../pages/usersList/usersList';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,22 +18,27 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
+<<<<<<< Updated upstream
   constructor(
     public platform: Platform,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen,
     private store: Store<fromRoot.State>
   ) {
+=======
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private authProvider: AuthProvider) {
+>>>>>>> Stashed changes
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'UsersList', component: UsersListPage }
     ];
 
   }
@@ -61,5 +69,10 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  onLogout() {
+    this.authProvider.logout();
+    this.nav.setRoot(LoginPage);
   }
 }
